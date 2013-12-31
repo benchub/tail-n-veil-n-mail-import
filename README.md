a6e68ce5ee1ab65905cfbe235322f2b8988d3cb2
Ben Chobot <bench@silentmedia.com>
2013-12-31 22:59:05 +0000

    Yay we have project!

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 tail-n-veil-n-mail-import
 =========================
 
-importer helper for tail-n-veil-n-mail
+importer helper for https://github.com/benchub/tail-n-veil-n-mail
+
+tail-n-veil-n-mail-import --help should be all that you need.
---
+++ import.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "regexp"
+  "strings"
+  "flag"
+  "encoding/json"
+)
+
+import (
+  _ "github.com/lib/pq"
+  "database/sql"
+)
+
+var configFileFlag = flag.String("config", "", "the config file")
+var bucketName = flag.String("bucketName", "", "which bucket this match will go to")
+var bucketFilter = flag.String("bucketFilter", "", "which regex will match this bucket")
+var testIt = flag.Bool("testData", true, "wait for test data on stdin")
+var eatIt = flag.Bool("eatIt", true, "on a match, don't pass the event on to other buckets")
+var reportIt = flag.Bool("reportIt", true, "record matches")
+var updateCounts = flag.Bool("updateCounts", true, "update filter usage count on matches")
+var apply = flag.Bool("apply", true, "apply bucket to unclassified events")
+type hostList []string
+var hostListFlag hostList
+
+type Configuration struct {
+  DBConn []string
+}
+
+// some functions to handle comma-separated arg lists
+func (list *hostList) String() string {
+  return fmt.Sprint(*list)
+}
+
+func (list *hostList) Set(value string) error {
+  for _, h := range strings.Split(value, ",") {
+    *list = append(*list, h)
+  }
+  return nil
+}
+
+func init() {
+  flag.Var(&hostListFlag, "onlyOn", "comma-separated list of hosts to restrict this filter to matching on")
+}
+
+func main() {
+  var db *sql.DB
+  
+  flag.Parse()
+  
+  if len(os.Args) == 1 {
+    flag.PrintDefaults()
+    os.Exit(0)
+  }
+
+  if *configFileFlag == "" {
+    fmt.Println("I need a config file!")
+    os.Exit(1)
+  } else {
+    configFile, err := os.Open(*configFileFlag)
+    if err != nil {
+      fmt.Println("opening config file:", err)
+      os.Exit(2)
+    }
+    
+    decoder := json.NewDecoder(configFile)
+    configuration := &Configuration{}
+    decoder.Decode(&configuration)
+    
+    db, err = sql.Open("postgres", configuration.DBConn[0])
+    if err != nil {
+      fmt.Println("couldn't connect to db", err)
+      os.Exit(2)
+    }
+  }
+  
+  if *bucketName == "" {
+    fmt.Println("I need a bucket name!")
+    os.Exit(1)
+  } 
+  
+  scanner := bufio.NewScanner(os.Stdin)
+
+  if *bucketFilter == "" {
+    // take the filter from stdin
+    fmt.Print("I need a bucket filter, so give me one now: ")
+    scanner.Scan()
+    line := scanner.Text()
+    if err := scanner.Err(); err != nil {
+      fmt.Println("couldn't read a filter from stdin", err)
+      os.Exit(2)
+    }
+    bucketFilter = &line
+  }
+
+  // verify the bucket filter is valid regex
+  re, err := regexp.Compile(*bucketFilter)
+  if err != nil {
+    fmt.Println("regex compile error for", *bucketFilter, err)
+    os.Exit(2)
+  }
+  
+  if *testIt {
+    fmt.Println("Give me a test line to make sure the regex you gave me does what you want it to:")
+    scanner.Scan()
+    line := scanner.Text()
+    if err := scanner.Err(); err != nil {
+      fmt.Println("couldn't read a filter from stdin", err)
+      os.Exit(2)
+    }
+
+    if matched := re.MatchString(line); !matched {
+      fmt.Println("Looks like your regex doesn't work for your test case!")
+      os.Exit(4)
+    }
+  }
+
+  tx, err := db.Begin()
+  if err != nil {
+    fmt.Println("ruh-oh, couldn't start transaction to insert new bucket filter", err)
+    os.Exit(3)
+  }
+
+  // Let's see if we already have this bucket.
+  // If we do, then we should just add this filter to the existing bucket. 
+  var id int32
+  err = db.QueryRow(`select id from buckets where name = $1`, *bucketName).Scan(&id)
+  switch {
+    default:
+      fmt.Println("Adding to bucket called \"", *bucketName, "\"using filter \"", *bucketFilter, "\"...")
+    case err == sql.ErrNoRows:
+      // If we don't, then make a new one.
+      err = db.QueryRow(`INSERT INTO buckets(name, eat_it, report_it) VALUES($1, $2, $3) RETURNING id`, *bucketName, *eatIt, *reportIt).Scan(&id)
+      if err != nil {
+        fmt.Println("couldn't insert new bucket", err)
+        os.Exit(3)
+      }
+      fmt.Println("Making a new bucket called \"", *bucketName, "\"using filter \"", *bucketFilter, "\"...")
+    case err != nil:
+        fmt.Println("couldn't search for existing bucket", err)
+        os.Exit(3)
+  }
+  if len(hostListFlag) > 0 {
+    fmt.Println(".... but only for", strings.Join(hostListFlag,", "))
+  }
+  _, err = db.Query(`INSERT INTO filters(bucket_id, filter, report) VALUES($1, $2, $3)`, id, *bucketFilter, *updateCounts)
+  if err != nil {
+    fmt.Println("couldn't insert new bucket filter", err)
+    os.Exit(3)
+  }
+  if len(hostListFlag) > 0 {
+    for h := 0; h < len(hostListFlag); h++ {
+      _, err := db.Query(`INSERT INTO onlyon(bucket_id, host) VALUES($1, $2)`, id, hostListFlag[h])
+      if err != nil {
+        fmt.Println("couldn't insert new bucket host restriction", err)
+        os.Exit(3)
+      }
+    }
+  }
+  
+  if *apply {
+    fmt.Println("Looking for unclassified matches to new filter... ")
+    
+    rows, err := db.Query(`select distinct event from events where bucket_id is null`)
+    if err != nil {
+      fmt.Println("couldn't find unclassified events", err)
+      os.Exit(3)
+    }
+    for rows.Next() {
+      var event string
+      if err = rows.Scan(&event); err != nil {
+        fmt.Println("couldn't read existing event", err)
+        os.Exit(3)
+      }
+      
+      // does this event match the new filter?
+      if matched := re.MatchString(event); matched {
+        // let's update the bucket_ids for this event
+        _, err := db.Query(`update events set bucket_id=$1 where bucket_id is null and event=$2`, id, event)
+        if err != nil {
+          fmt.Println("couldn't updated matching events", err)
+          os.Exit(3)
+        }
+      }
+    }
+    if err = rows.Err(); err != nil {
+      fmt.Println("couldn't walk rows while looking for matching events", err)
+      os.Exit(3)
+    }
+    
+    rows.Close()
+
+  }
+
+  err = tx.Commit()
+  if err != nil {
+    fmt.Println("ruh-oh, couldn't commit transaction to insert new bucket", err)
+    os.Exit(3)
+  }
+
+  fmt.Println("... done!")
+  
+  db.Close();
+}
+
